f98fab4be8ac7ad1c6426b7de0eabcd194545cb4
nyvalldo <serafim739@gmail.com>
2022-11-19 18:40:34 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,8 +9,7 @@ public class Program {
 	public static void main(String[] args) {
 	
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPiece());
-		
+		UI.printBoard(chessMatch.getPiece());	
 		
 	}
 }
